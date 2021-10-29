cf6a3f99d0c685cadbcbea2c6a56d88e1d8a9558
RazOre1st <88528720+RazOre1st@users.noreply.github.com>
2021-10-29 02:18:29 -0700

    Add files via upload

---
+++ EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+public GameObject Enemy;
+{	
+    // Start is called before the first frame update
+    void Start()
+    {
+		StartCoroutine(StartSpawning());
+	}
+		IEnumerator Start Spawning()
+		{
+			yield return new WaitForSeonds(Random.Range(7f, 10f);
+				StartCoroutine(StartSpawning());
+		}
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
